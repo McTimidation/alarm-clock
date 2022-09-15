17990be71a89d1d4bc20e174eab9b17a9ebcb592
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-09-15 02:46:32 +0000

    working clock

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 
 const time = document.getElementById('time');
 
-time.textContent = "I am a clock";
+ 
+function getTime() {
+    const date = new Date();// AHA MOMENT! - Declaring this outside of your function will cause your function 
+                            //          to reference the time the const is declared
+    time.textContent = date.toLocaleTimeString();
+}
+
+setInterval(getTime, 1000);
+
 
 
 
