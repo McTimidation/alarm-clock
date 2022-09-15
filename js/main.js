42c1f5e9722801ecf0e892c0fd8d8785ec2aa6d8
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-09-15 02:50:45 +0000

    working alarm alert with if statement

--- js/main.js
+++ js/main.js
@@ -6,7 +6,12 @@ function getTime() {
     const date = new Date();// AHA MOMENT! - Declaring this outside of your function will cause your function 
                             //          to reference the time the const is declared
     time.textContent = date.toLocaleTimeString();
+    if (time.textContent === '10:50:00 PM'){
+        alert("It's 10:50!")
+    }
 }
+
+
 
 setInterval(getTime, 1000);
 
